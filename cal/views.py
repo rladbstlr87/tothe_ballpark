2efa01f22b17ea2fde30f161d02004c87001ff5b
seongjin <seongjin5743@naver.com>
2025-05-21 15:22:55 +0900

    qwer

--- cal/views.py
+++ cal/views.py
@@ -1,34 +1,52 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 from datetime import datetime, timedelta, date
 from django.utils.safestring import mark_safe
 import calendar
+import csv
+from django.conf import settings
 from .models import *
 from .utils import Calendar
 
-# 메인 페이지
 def index(request):
     return render(request, 'index.html')
 
-# 함수형 뷰로 calendar 페이지 구현
 def calendar_view(request):
+    user_team = None
+    if request.user.is_authenticated:
+        user_team = request.user.team  # 로그인한 사용자의 팀
+
+        # 기존 일정 삭제 후 새로 저장
+        Game.objects.all().delete()
+        csv_file_path = settings.BASE_DIR / 'data' / 'kbo_schedule.csv'
+        with open(csv_file_path, newline='', encoding='utf-8-sig') as csvfile:
+            reader = csv.DictReader(csvfile)
+            for row in reader:
+                if row['team'] == user_team:
+                    game_date = datetime.strptime(row['day'], '%Y-%m-%d').date()
+                    game = Game(
+                        date=game_date,
+                        home_team=row['team'],
+                        away_team=row['opponent'],
+                        stadium=row['stadium'],
+                        time=row['time'],
+                        play=row['play'],
+                        note=row['note'],
+                        result=row['result']
+                    )
+                    game.save()
+
     d = get_date(request.GET.get('day', None))
-    cal = Calendar(d.year, d.month)
+    cal = Calendar(d.year, d.month, team=user_team)  # team 전달
     html_cal = cal.formatmonth(withyear=True)
+
     context = {
         'calendar': mark_safe(html_cal),
         'prev_month': prev_month(d),
         'next_month': next_month(d),
-        # 필요하다면 추가 context
+        'user_team': user_team,
     }
-    if request.user.is_authenticated:
-        user_team = request.user.team
-    print(f'[DEBUG] user_team={user_team}')
-
-    
     return render(request, 'cal/calendar.html', context)
 
-# url에서 전달된 년도와 월 정보를 추출하여, datetime 객체로 변환하는 함수
 def get_date(req_day):
     try:
         if req_day:
@@ -38,14 +56,12 @@ def get_date(req_day):
         pass
     return datetime.today().date()
 
-# 주어진 날짜의 이전 달을 계산
 def prev_month(d):
     first = d.replace(day=1)
     prev_month = first - timedelta(days=1)
     a = 'day=' + str(prev_month.year) + '-' + str(prev_month.month) + '-' + str(prev_month.day)
     return a
 
-# 주어진 날짜의 다음 달을 계산
 def next_month(d):
     days_in_month = calendar.monthrange(d.year, d.month)[1]
     last = d.replace(day=days_in_month)
