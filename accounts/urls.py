bee57aa0e6505bd103a21b2a8df36af26897eb13
민예린 <167814578+Yerineeeee98@users.noreply.github.com>
2025-05-23 16:29:41 +0900

    회원가입/ 로그인 꾸미기 1차

--- accounts/urls.py
+++ accounts/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('signup/', views.signup, name='signup'),  # 회원가입 페이지 URL
     path('login/', views.login, name='login'),  # 로그인 페이지 URL
     path('logout/', views.logout, name='logout'),  # 로그아웃 처리 URL
+    path('auth/', views.auth_view, name='auth'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -8,6 +8,7 @@ from .models import User  # User 모델 가져오기
 from django.contrib.auth.decorators import login_required  # 로그인 여부를 확인하는 데코레이터
 from cal import views  # cal 앱의 views 모듈 가져오기
 
+
 # 회원가입 뷰
 def signup(request):
     if request.method == 'POST':  # POST 요청일 경우, 폼 데이터를 처리
@@ -47,3 +48,39 @@ def logout(request):
 # 메인 페이지 뷰
 def home(request):
     return redirect('/')
+
+# 회원가입과 로그인 전환 뷰 
+def auth_view(request):
+    mode = request.GET.get('mode', 'login')  
+    # url 쿼리스트링에서 mode라는 값을 읽어옴(?mode=signup) 
+    # 만약 없으면 기본값으로 login을 설정
+    # 화면에서 로그인 폼을 먼저 보여줄지, 회원가입 폼을 먼저 보여줄지 결정 
+
+    if request.method == 'POST': # 폼 제출을 했는지 여부( post면 사용자가 로그인 또는 회원가입 폼을 제출했다는 의미미)
+        if request.POST.get('action') == 'signup': # 회원가입 일 경우우
+            mode = 'signup'  # 화면에서 회원가입 폼을 보여주도록 mode값을 'signup'으로 지정정
+            signup_form = CustomUserCreationForm(request.POST) # 회원가입 폼 데이터로 인스턴스 생성성
+            login_form = CustomAuthenticationForm() # 로그인 폼은 비워진 상태로 준비(회원가입 폼만 처리 중중)
+            if signup_form.is_valid(): # 회원가입 폼이 유효한지 검사 
+                user = signup_form.save() # 폼에 입력된 내용을 바탕으로 실제 회원 데이터 저장장
+                auth_login(request, user)
+                return redirect('cal:calendar') # 회원가입하고 바로 캘린더로 이동
+        elif request.POST.get('action') == 'login': #로그인시 
+            mode = 'login'
+            login_form = CustomAuthenticationForm(request, data=request.POST)
+            signup_form = CustomUserCreationForm()
+            if login_form.is_valid():
+                auth_login(request, login_form.get_user()) # 로그인 성공 시 세션에 사용자 정보를 저장해 로그인 처리리
+                return redirect('cal:calendar') # 로그인 성공시 캘린더로 이동동
+        else: # 비정상 post일 경우(action이 signup도 아니고 login도 아닐경우)
+            signup_form = CustomUserCreationForm() # 로그인과 회원가입 폼 모두 빈 상태로 초기화 
+            login_form = CustomAuthenticationForm()
+    else: # 요청이 post가 아니면 (즉 get요청, 페이지 최초 접속 시)
+        signup_form = CustomUserCreationForm()
+        login_form = CustomAuthenticationForm()
+
+    return render(request, 'auth.html', {       # 최종적으로 auth.html 템플릿에 준비한 폼들과 mode 변수를 넘겨서 렌더링링
+        'signup_form': signup_form,
+        'login_form': login_form,
+        'mode': mode, # mode에 따라 로그인 폼 또는 회원가입 폼 중 어떤 걸 기본으로 보여줄지 결정 
+    })
