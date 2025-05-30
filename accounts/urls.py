1a0f8a45b3ae0cbf54362c52268f3aab031470a5
seongjin <seongjin5743@naver.com>
2025-05-30 14:36:19 +0900

    라인업 선수 상세정보, 직관여부 자바 스크립트

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,8 +4,6 @@ from . import views
 app_name = 'accounts'
 
 urlpatterns = [
-    path('signup/', views.signup, name='signup'),  # 회원가입 페이지 URL
-    path('login/', views.login, name='login'),  # 로그인 페이지 URL
     path('logout/', views.logout, name='logout'),  # 로그아웃 처리 URL
     path('auth/', views.auth_view, name='auth'),
 ]
