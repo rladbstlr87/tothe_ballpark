54720bc57b12e879c1ece3f9250f4d0d4ce72686
seongjin <seongjin5743@naver.com>
2025-06-17 15:11:01 +0900

    기타

---
+++ after_game.sh
@@ -0,0 +1,7 @@
+echo " 데이터 로딩 커맨드 실행 중..."
+python manage.py games
+python manage.py hitters
+python manage.py pitchers
+python manage.py lineup
+python manage.py daily_hitter_stat
+python manage.py daily_pitcher_stat
---
+++ before_game.sh
@@ -0,0 +1,3 @@
+echo " 데이터 로딩 커맨드 실행 중..."
+python manage.py games
+python manage.py lineup
