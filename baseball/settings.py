7b7f8ef1f564e5047c6ed148f0876d98a450ba92
seongjin <seongjin5743@naver.com>
2025-06-18 14:33:23 +0900

    collectstatic

--- baseball/settings.py
+++ baseball/settings.py
@@ -126,6 +126,8 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
 
+STATIC_ROOT = 'staticfiles/'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
