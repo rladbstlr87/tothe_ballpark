ab6d637fe6014fd0b1b311fee4570722838a30cb
Ubuntu <ubuntu@ip-172-31-1-47.ap-northeast-2.compute.internal>
2025-06-24 06:10:59 +0000

    삭제

--- before_game.sh
+++
@@ -1,5 +0,0 @@
-source /mnt/c/Users/seong/KBO/uvenv/bin/activate
-
-python /mnt/c/Users/seong/KBO/data/make_stat/06_lineup.py
-python /mnt/c/Users/seong/KBO/manage.py games
-python /mnt/c/Users/seong/KBO/manage.py lineup
