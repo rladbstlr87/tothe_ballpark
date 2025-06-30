14b5d57c4fb6956f565a91b5d6f086dfe8dca3e5
seongjin <seongjin5743@naver.com>
2025-06-30 11:18:06 +0900

    데이터 전처리

--- data/make_stat/04_player_style.py
+++ data/make_stat/04_player_style.py
@@ -5,16 +5,3 @@ p['style'] = p.apply(pitcher_style, axis=1)
 
 h.to_csv('data/all_hitter_stats.csv', index=False)
 p.to_csv('data/all_pitcher_stats.csv', index=False)
-
-print((h['style'] == 0).sum())
-print((h['style'] == 1).sum())
-print((h['style'] == 2).sum())
-print((h['style'] == 3).sum())
-print((h['style'] == 4).sum())
-print('')
-print((p['style'] == 0).sum())
-print((p['style'] == 1).sum())
-print((p['style'] == 2).sum())
-print((p['style'] == 3).sum())
-
-print(h.loc[h['style'] == 1, 'player_name'].to_list())
