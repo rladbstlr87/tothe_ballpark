0be44cb29c0c2d0696aa84d9c7cbaebbb1676528
rladbstlr87 <manylong@naver.com>
2025-08-14 18:59:06 +0900

    로컬 개발환경 접속 수정

--- baseball/settings/base.py
+++ baseball/settings/base.py
@@ -87,6 +87,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'accounts.User'
