0e0dae0cf6442fa152b809d3f5aac9f2621febb5
rladbstlr87 <manylong@naver.com>
2025-08-24 16:33:57 +0900

    운영 세팅 db설정

--- baseball/settings/prod.py
+++ baseball/settings/prod.py
@@ -33,3 +33,15 @@ CSRF_TRUSTED_ORIGINS = [
 ]
 
 WHITENOISE_MANIFEST_STRICT = True
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": config('DB_NAME'),
+        "USER": config('DB_USER'),
+        "PASSWORD": config('DB_PASSWORD'),
+        "HOST": config('DB_HOST'),
+        "PORT": config('DB_PORT'),
+        "OPTIONS": {"sslmode": "require"},
+    }
+}
