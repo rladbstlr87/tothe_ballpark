9bfd9479c48e415031a9289c3012d2ecd6d8e729
rladbstlr87 <manylong@naver.com>
2025-08-27 22:41:11 +0900

    update

--- baseball/settings/base.py
+++ baseball/settings/base.py
@@ -27,7 +27,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework',
+    # 'rest_framework',
     'cal',
     'accounts',
     'posts',
