e6e4f2d5af99bf6d0e9b2c48134e53edb90680b3
rladbstlr87 <manylong@naver.com>
2025-11-25 22:08:37 +0900

    점프조인 적용으로 N+1문제 해결

--- cal/views.py
+++ cal/views.py
@@ -239,8 +239,7 @@ def calculate_pitcher_score(p):
 @login_required
 def lineup(request, game_id):
     game = get_object_or_404(Game, id=game_id)
-    # TODO: Use select_related to prefetch pitcher/hitter and avoid N+1 in templates (perf step 4)
-    lineups = Lineup.objects.filter(game=game).order_by('id')
+    lineups = Lineup.objects.select_related('pitcher', 'hitter').filter(game=game).order_by('id')
     has_lineup = lineups.exists()
 
     user_lineup = []
